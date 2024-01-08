10a1115e1c9e639babe1094795a8e3b93d3d9ea5
maksimaleksev <59773630+maksimaleksev@users.noreply.github.com>
2024-01-08 17:51:46 +0300

    TaskManagerPackageTests added

---
+++ Tests/TaskManagerPackageTests/TaskManagerPackageTests.swift
@@ -0,0 +1,13 @@
+//
+//  TaskManagerPackageTests.swift
+//
+//
+//  Created by Maxim Alekseev on 08.01.2024.
+//
+
+import Foundation
+
+import XCTest
+@testable import TaskManagerPackage
+
+final class TaskManagerPackageTests: XCTestCase { }
